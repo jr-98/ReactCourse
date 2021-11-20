d4d68d8f46fb61a0d4a8acb5716dab125b0cf200
Jonathan Tillaguango <jonathan.tillaguango@unl.edu.ec>
2021-11-20 02:39:46 -0500

    pre-deploy

--- crud-fulbol/src/App.js
+++ crud-fulbol/src/App.js
@@ -2,326 +2,294 @@ import React from "react";
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import {
-    Table,
-    Button,
-    Container,
-    Modal,
-    ModalHeader,
-    ModalBody,
-    FormGroup,
-    ModalFooter,
+  Table,
+  Button,
+  Container,
+  Modal,
+  ModalHeader,
+  ModalBody,
+  FormGroup,
+  ModalFooter,
 } from "reactstrap";
+import Data from "./Modules/Data";
 
-const data = [{
-    id: 1,
-    jugador: 'Leonel Andres Messi',
-    dorsal: 30,
-    equipo: 'Paros Saint-Ger Man'
-},
-{
-    id: 2,
-    jugador: 'Cristiano Ronaldo dos Santos Aveiro',
-    dorsal: 7,
-    equipo: 'Mancherter United'
-},
-{
-    id: 3,
-    jugador: 'Kilian Mbape',
-    dorsal: 7,
-    equipo: 'Paros Saint-Ger Man'
-},
-{
-    id: 4,
-    jugador: 'Erling Halan',
-    dorsal: '7',
-    equipo: 'Borissia Dormund'
-},
-{
-    id: 5,
-    jugador: 'Neymar Junior',
-    dorsal: 10,
-    equipo: 'Paros Saint-Ger Man'
-},
-{
-    id: 6,
-    jugador: 'Robert Lewandowski',
-    dorsal: 9,
-    equipo: 'Bayer Munich'
-},
-]
-
+const data = Data
 class App extends React.Component {
-    state = {
-        data: data,
-        modalActualizar: false,
-        modalInsertar: false,
-        form: {
-            id: '',
-            jugador: '',
-            dorsal: '',
-            equipo: '',
-        },
-    };
+  state = {
+    data: data,
+    createModal: false,
+    updateModal: false,
+    form: {
+      id: '',
+      jugador: '',
+      dorsal: '',
+      equipo: '',
+    },
+  };
+  // Show ans hide Create Modal
+  showcreateModal = () => {
+    this.setState({
+      createModal: true,
+    });
+  };
 
-    mostrarModalActualizar = (dato) => {
-        this.setState({
-            form: dato,
-            modalActualizar: true,
-        });
-    };
+  hidecreateModall = () => {
+    this.setState({ createModal: false });
+  };
+  // Show ans hide Update Modal
+  showUpdateModal = (dato) => {
+    this.setState({
+      form: dato,
+      updateModal: true,
+    });
+  };
 
-    cerrarModalActualizar = () => {
-        this.setState({ modalActualizar: false });
-    };
+  hideUpdateModal = () => {
+    this.setState({ updateModal: false });
+  };
 
-    mostrarModalInsertar = () => {
-        this.setState({
-            modalInsertar: true,
-        });
-    };
+  // Create new players 
+  create = () => {
+    var newValue = { ...this.state.form };
+    newValue.id = this.state.data.length + 1;
+    var newPlayersList = this.state.data;
+    newPlayersList.push(newValue);
+    this.setState({ createModal: false, data: newPlayersList });
+  }
 
-    cerrarModalInsertar = () => {
-        this.setState({ modalInsertar: false });
-    };
+  // Update players list
+  update = (updateData) => {
+    var count = 0;
+    var editList = this.state.data;
+    editList.map((registro) => {
+      if (updateData.id == registro.id) {
+        editList[count].jugador = updateData.jugador;
+        editList[count].dorsal = updateData.dorsal
+        editList[count].equipo = updateData.equipo;
+      }
+      count++;
+    });
+    this.setState({ data: editList, updateModal: false });
+  };
 
-    editar = (dato) => {
-        var contador = 0;
-        var arreglo = this.state.data;
-        arreglo.map((registro) => {
-            if (dato.id == registro.id) {
-                arreglo[contador].jugador = dato.jugador;
-                arreglo[contador].dorsal = dato.dorsal
-                arreglo[contador].equipo = dato.equipo;
-            }
-            contador++;
-        });
-        this.setState({ data: arreglo, modalActualizar: false });
-    };
-
-    eliminar = (dato) => {
-        var opcion = window.confirm("Estás Seguro que deseas Eliminar el elemento " + dato.id);
-        if (opcion == true) {
-            var contador = 0;
-            var arreglo = this.state.data;
-            arreglo.map((registro) => {
-                if (dato.id == registro.id) {
-                    arreglo.splice(contador, 1);
-                }
-                contador++;
-            });
-            this.setState({ data: arreglo, modalActualizar: false });
+  //Delete register
+  eliminar = (deleteData) => {
+    var option = window.confirm("Estás Seguro que deseas Eliminar el elemento " + deleteData.id);
+    if (option == true) {
+      var count = 0;
+      var playersList = this.state.data;
+      playersList.map((register) => {
+        if (deleteData.id == register.id) {
+          playersList.splice(count, 1);
         }
-    };
-
-    insertar = () => {
-        var valorNuevo = { ...this.state.form };
-        valorNuevo.id = this.state.data.length + 1;
-        var lista = this.state.data;
-        lista.push(valorNuevo);
-        this.setState({ modalInsertar: false, data: lista });
+        count++;
+      });
+      this.setState({ data: playersList, updateModal: false });
     }
+  };
 
-    handleChange = (e) => {
-        this.setState({
-            form: {
-                ...this.state.form,
-                [e.target.name]: e.target.value,
-            },
-        });
-    };
+  handleChange = (e) => {
+    this.setState({
+      form: {
+        ...this.state.form,
+        [e.target.name]: e.target.value,
+      },
+    });
+  };
 
-    render() {
+  render() {
 
-        return (
-            <>
-                <Container>
-                    <br />
-                    <Button color="success" onClick={() => this.mostrarModalInsertar()}>Crear</Button>
-                    <br />
-                    <br />
-                    <Table>
-                        <thead>
-                            <tr>
-                                <th>ID</th>
-                                <th>jugador</th>
-                                <th>dorsal</th>
-                                <th>equipo</th>
-                            </tr>
-                        </thead>
+    return (
+      <>
+        <Container>
+          <br />
+          <Button color="success" onClick={() => this.showcreateModal()}>Crear</Button>
+          <br />
+          <br />
+          <Table striped bordered hover size="sm">
+            <thead >
+              <tr>
+                <th>ID</th>
+                <th>Jugador</th>
+                <th>Dorsal</th>
+                <th>Equipo</th>
+                <th>Operaciones</th>
+              </tr>
+            </thead>
 
-                        <tbody>
-                            {this.state.data.map((dato) => (
-                                <tr key={dato.id}>
-                                    <td>{dato.id}</td>
-                                    <td>{dato.jugador}</td>
-                                    <td>{dato.dorsal}</td>
-                                    <td>{dato.equipo}</td>
-                                    <td>
-                                        <Button
-                                            color="primary"
-                                            onClick={() => this.mostrarModalActualizar(dato)}
-                                        >
-                                            Editar
-                                        </Button>{" "}
-                                        <Button color="danger" onClick={() => this.eliminar(dato)}>Eliminar</Button>
-                                    </td>
-                                </tr>
-                            ))}
-                        </tbody>
-                    </Table>
-                </Container>
+            <tbody>
+              {this.state.data.map((dato) => (
+                <tr key={dato.id}>
+                  <td>{dato.id}</td>
+                  <td>{dato.jugador}</td>
+                  <td>{dato.dorsal}</td>
+                  <td>{dato.equipo}</td>
+                  <td>
+                    <Button
+                      color="primary"
+                      onClick={() => this.showUpdateModal(dato)}
+                    >
+                      Editar
+                    </Button>{" "}
+                    <Button color="danger" onClick={() => this.eliminar(dato)}>Eliminar</Button>
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </Table>
+        </Container>
 
-                <Modal isOpen={this.state.modalActualizar}>
-                    <ModalHeader>
-                        <div><h3>Editar Registro</h3></div>
-                    </ModalHeader>
+        <Modal isOpen={this.state.updateModal}>
+          <ModalHeader>
+            <div><h3>Editar Registro</h3></div>
+          </ModalHeader>
 
-                    <ModalBody>
-                        <FormGroup>
-                            <label>
-                                Id:
-                            </label>
+          <ModalBody>
+            <FormGroup>
+              <label>
+                Id:
+              </label>
 
-                            <input
-                                className="form-control"
-                                readOnly
-                                type="text"
-                                value={this.state.form.id}
-                            />
-                        </FormGroup>
+              <input
+                className="form-control"
+                readOnly
+                type="text"
+                value={this.state.form.id}
+              />
+            </FormGroup>
 
-                        <FormGroup>
-                            <label>
-                                jugador:
-                            </label>
-                            <input
-                                className="form-control"
-                                name="jugador"
-                                type="text"
-                                onChange={this.handleChange}
-                                value={this.state.form.jugador}
-                            />
-                        </FormGroup>
+            <FormGroup>
+              <label>
+                Jugador:
+              </label>
+              <input
+                className="form-control"
+                name="jugador"
+                type="text"
+                onChange={this.handleChange}
+                value={this.state.form.jugador}
+              />
+            </FormGroup>
 
-                        <FormGroup>
-                            <label>
-                                dorsal:
-                            </label>
-                            <input
-                                className="form-control"
-                                name="dorsal"
-                                type="number"
-                                onChange={this.handleChange}
-                                value={this.state.form.dorsal}
-                            />
-                        </FormGroup>
+            <FormGroup>
+              <label>
+                Dorsal:
+              </label>
+              <input
+                className="form-control"
+                name="dorsal"
+                type="number"
+                onChange={this.handleChange}
+                value={this.state.form.dorsal}
+              />
+            </FormGroup>
 
-                        <FormGroup>
-                            <label>
-                                equipo:
-                            </label>
-                            <input
-                                className="form-control"
-                                name="equipo"
-                                type="text"
-                                onChange={this.handleChange}
-                                value={this.state.form.equipo}
-                            />
-                        </FormGroup>
-                    </ModalBody>
+            <FormGroup>
+              <label>
+                Equipo:
+              </label>
+              <input
+                className="form-control"
+                name="equipo"
+                type="text"
+                onChange={this.handleChange}
+                value={this.state.form.equipo}
+              />
+            </FormGroup>
+          </ModalBody>
 
-                    <ModalFooter>
-                        <Button
-                            color="primary"
-                            onClick={() => this.editar(this.state.form)}
-                        >
-                            Editar
-                        </Button>
-                        <Button
-                            color="danger"
-                            onClick={() => this.cerrarModalActualizar()}
-                        >
-                            Cancelar
-                        </Button>
-                    </ModalFooter>
-                </Modal>
+          <ModalFooter>
+            <Button
+              color="primary"
+              onClick={() => this.update(this.state.form)}
+            >
+              Editar
+            </Button>
+            <Button
+              color="danger"
+              onClick={() => this.hideUpdateModal()}
+            >
+              Cancelar
+            </Button>
+          </ModalFooter>
+        </Modal>
 
 
 
-                <Modal isOpen={this.state.modalInsertar}>
-                    <ModalHeader>
-                        <div><h3>Insertar jugador</h3></div>
-                    </ModalHeader>
+        <Modal isOpen={this.state.createModal}>
+          <ModalHeader>
+            <div><h3>Insertar jugador</h3></div>
+          </ModalHeader>
 
-                    <ModalBody>
-                        <FormGroup>
-                            <label>
-                                Id:
-                            </label>
+          <ModalBody>
+            <FormGroup>
+              <label>
+                Id:
+              </label>
 
-                            <input
-                                className="form-control"
-                                readOnly
-                                type="text"
-                                value={this.state.data.length + 1}
-                            />
-                        </FormGroup>
+              <input
+                className="form-control"
+                readOnly
+                type="text"
+                value={this.state.data.length + 1}
+              />
+            </FormGroup>
 
-                        <FormGroup>
-                            <label>
-                                jugador:
-                            </label>
-                            <input
-                                className="form-control"
-                                name="jugador"
-                                type="text"
-                                onChange={this.handleChange}
-                            />
-                        </FormGroup>
+            <FormGroup>
+              <label>
+                Jugador:
+              </label>
+              <input
+                className="form-control"
+                name="jugador"
+                type="text"
+                onChange={this.handleChange}
+              />
+            </FormGroup>
 
-                        <FormGroup>
-                            <label>
-                                dorsal:
-                            </label>
-                            <input
-                                className="form-control"
-                                name="dorsal"
-                                type="number"
-                                onChange={this.handleChange}
-                                value={this.state.form.dorsal}
-                            />
-                        </FormGroup>
+            <FormGroup>
+              <label>
+                Dorsal:
+              </label>
+              <input
+                className="form-control"
+                name="dorsal"
+                type="number"
+                onChange={this.handleChange}
+                value={this.state.form.dorsal}
+              />
+            </FormGroup>
 
-                        <FormGroup>
-                            <label>
-                                equipo:
-                            </label>
-                            <input
-                                className="form-control"
-                                name="equipo"
-                                type="text"
-                                onChange={this.handleChange}
-                            />
-                        </FormGroup>
-                    </ModalBody>
+            <FormGroup>
+              <label>
+                Equipo:
+              </label>
+              <input
+                className="form-control"
+                name="equipo"
+                type="text"
+                onChange={this.handleChange}
+              />
+            </FormGroup>
+          </ModalBody>
 
-                    <ModalFooter>
-                        <Button
-                            color="primary"
-                            onClick={() => this.insertar()}
-                        >
-                            Insertar
-                        </Button>
-                        <Button
-                            className="btn btn-danger"
-                            onClick={() => this.cerrarModalInsertar()}
-                        >
-                            Cancelar
-                        </Button>
-                    </ModalFooter>
-                </Modal>
-            </>
-        );
-    }
+          <ModalFooter>
+            <Button
+              color="primary"
+              onClick={() => this.create()}
+            >
+              Insertar
+            </Button>
+            <Button
+              className="btn btn-danger"
+              onClick={() => this.hidecreateModall()}
+            >
+              Cancelar
+            </Button>
+          </ModalFooter>
+        </Modal>
+      </>
+    );
+  }
 }
 export default App;
---
+++ crud-fulbol/src/Modules/Data.jsx
@@ -0,0 +1,40 @@
+const Data = [
+    {
+        id: 1,
+        jugador: 'Lionel Andrés Messi Cuccittini',
+        dorsal: 30,
+        equipo: 'Paris Saint-Germain'
+    },
+    {
+        id: 2,
+        jugador: 'Cristiano Ronaldo dos Santos Aveiro',
+        dorsal: 7,
+        equipo: 'Mancherter United'
+    },
+    {
+        id: 3,
+        jugador: 'Kilian Mbape',
+        dorsal: 7,
+        equipo: 'Paros Saint-Ger Man'
+    },
+    {
+        id: 4,
+        jugador: 'Erling Halan',
+        dorsal: '7',
+        equipo: 'Borissia Dormund'
+    },
+    {
+        id: 5,
+        jugador: 'Neymar Junior',
+        dorsal: 10,
+        equipo: 'Paros Saint-Ger Man'
+    },
+    {
+        id: 6,
+        jugador: 'Robert Lewandowski',
+        dorsal: 9,
+        equipo: 'Bayer Munich'
+    },
+]
+// Datos de prueba
+export default Data;
