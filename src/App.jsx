672b16fddc014c626158d4c758eb65f7cb409213
jr-98 <jrtillaguango@gmail.com>
2022-08-30 10:56:55 -0500

    React Router dom Link and BrouserRouter

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,14 @@
-import logo from './logo.svg';
 import './App.css';
-
+import { Link, BrowserRouter as Router } from 'react-router-dom'
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <Link to={"/customers"}>Customers</Link>
+        <br />
+        <Link to={"/customers/3000000"}>Customers 30 0000 0000 </Link>
+      </div>
+    </Router>
   );
 }
 
