acbb6fb4335777e583b0122389ce0794329ffdb8
jr-98 <jrtillaguango@gmail.com>
2022-09-08 01:04:05 -0500

    Instalacion y uso de json-server usando del server

--- src/actions/fetchCustomer.jsx
+++ src/actions/fetchCustomer.jsx
@@ -1,28 +1,7 @@
 import { createAction } from 'redux-actions';
 import { FETCH_CUSTOMERS } from "../constants";
 
-const customers = [
-    {
-        "dni": '27000000',
-        "name": 'Juan Perez',
-        "age": 37
-    },
-    {
-        "dni": '1104316548',
-        "name": 'Evelyn Alexandra',
-        "age": 24
-    },
-    {
-        "dni": '3209320932',
-        "name": 'Bambestre Rosalia',
-        "age": 34
-    },
-    {
-        "dni": '3209343442',
-        "name": 'Totis del Carmen ',
-        "age": 34
-    },
-
-]
+const url = 'http://localhost:3001/customers';
+const apiFetchCustomers = () => fetch(url).then(res => res.json());
 //export const fetchCustomer = () => ({ type: FETCH_CUSTOMERS, payload: null })
-export const fetchCustomer = createAction(FETCH_CUSTOMERS, () => customers);
+export const fetchCustomer = createAction(FETCH_CUSTOMERS, apiFetchCustomers);
--- src/store/index.jsx
+++ src/store/index.jsx
@@ -1,9 +1,8 @@
-import { compose, createStore } from 'redux';
+import { applyMiddleware, compose, createStore } from 'redux';
 import reducers from '../reducers';
-
+import promiseMiddleware from 'redux-promise';
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 export const store = createStore(reducers,
     {},
-    composeEnhancers()
-
+    composeEnhancers(applyMiddleware(promiseMiddleware))
 );
